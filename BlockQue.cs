235a54fed76bd6d4cf0039b6b153e61d956e5092
Arch13N <noah-bordasch@web.de>
2023-03-01 12:18:19 +0100

    Added Block classes

---
+++ BlockQue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class BlockQue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock(),
+        };
+
+    }
+}
